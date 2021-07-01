7abf066eef915e1d33866c88851853f8fe779be5
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-07-01 11:57:46 +0300

    Add except control

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -3,7 +3,40 @@
  *
  * @returns void
  */
-export function clearStorage(): void {
-  localStorage.clear();
-  sessionStorage.clear();
+export function clearStorage(except?: string | string[]): void {
+  if (except) {
+    switch (typeof except) {
+      case 'string':
+        removeFromStorage(except);
+        break;
+      case 'object':
+        except.forEach((exceptedKey) => {
+          removeFromStorage(exceptedKey);
+        });
+        break;
+    }
+  } else {
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+}
+
+function removeFromStorage(except: string) {
+  for (let i = 0, len = localStorage.length; i < len; i++) {
+    const removeableKey = localStorage.key(i) ?? '';
+    if (!removeableKey.includes(except)) {
+      removeItemFromStorage(localStorage, removeableKey);
+    }
+  }
+
+  for (let i = 0, len = sessionStorage.length; i < len; i++) {
+    const removeableKey = sessionStorage.key(i) ?? '';
+    if (!removeableKey.includes(except)) {
+      removeItemFromStorage(sessionStorage, removeableKey);
+    }
+  }
+}
+
+function removeItemFromStorage(storage: Storage, key: string): void {
+  storage.removeItem(key);
 }
