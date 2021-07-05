f2675a3879586a42fbe093b180cf8babdf3e4318
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-07-05 17:29:41 +0300

    Add new local storage tests

--- test/storage/local/index.test.ts
+++ test/storage/local/index.test.ts
@@ -9,6 +9,30 @@ describe('toLocalStorage', () => {
   it('should be defined', () => {
     expect(toLocalStorage).toBeDefined();
   });
+
+  it('should be adds new item with non existing key', () => {
+    const key = 'StorageKey';
+    const value = 'Storage Value';
+
+    toLocalStorage(key, value);
+    const result = fromLocalStorage(key);
+
+    expect(JSON.parse(result)).toEqual(value);
+  });
+
+
+  it("should be updated existing key value", () => {
+    const key = "StorageKey";
+    const value = 'Storage Value';
+    const newValue = "New storage value";
+    toLocalStorage(key, value);
+
+    toLocalStorage(key, newValue);
+    const result = fromLocalStorage(key);
+    
+    expect(JSON.parse(result)).toEqual(newValue);
+});
+
 });
 
 describe('fromLocalStorage', () => {
@@ -27,4 +51,29 @@ describe('clearLocalStorage', () => {
   it('should be defined', () => {
     expect(clearLocalStorage).toBeDefined();
   });
+});
+
+// Mock localStorage
+interface IStorage {
+  [key: string]: string;
+}
+
+const localStorageMock = (() => {
+  let store: IStorage = {};
+
+  return {
+    getItem(key: string) {
+      return store[key] || null;
+    },
+    setItem(key: string, value: any) {
+      store[key] = value;
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
 });
