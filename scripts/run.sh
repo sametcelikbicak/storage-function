12dfc2100cc89e7e1ad968e24b5282111764ab71
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2022-08-05 12:01:49 +0300

    Update script and version

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "Hello, "$USER".\nWelcome to the storage-function build script!"
-echo -e "Which version option do you wish to use?\n1. Major [1.x.x]\n2. Minor [x.1.x]\n3. Patch [x.x.1] (Default)"
+echo -e "Which version option do you wish to use?\n1. Major [1.x.x]\n2. Minor [x.1.x]\n3. Patch [x.x.1] (Default)\n4. Manuel [x.x.x]"
 echo -n "Enter your choice [1-2-3] or press [ENTER]:"
 read choice
 
@@ -11,6 +11,11 @@ if [ $choice == 1 ]; then
 elif [ $choice == 2 ]; then
   echo "Minor version is preparing..."
   npm version minor
+elif [ $choice == 4 ]; then
+  echo -n "Enter your new version with [x.x.x] format:"
+  read version
+  echo "$version version is preparing..."
+  npm version $version
 else
   echo "Patch version is preparing..."
   npm version patch
